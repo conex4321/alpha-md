ddf16221de22f08de8fc78229c076b4f83d9e22d
Obasuyi Promise <156423967+C-iph3r@users.noreply.github.com>
2024-06-22 11:56:23 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,67 +1,67 @@
-const toBool = (x) => x == "true";
-const { existsSync } = require("fs");
 const { Sequelize } = require("sequelize");
-if (existsSync('config.env')) require('dotenv').config({ path: './.env' })
-process.env.NODE_OPTIONS = "--max_old_space_size=2560"; //2.5
-const DB_URL = process.env.DATABASE_URL || "";
-const keysInrl = ['hjS2WBZ', 'QbfLgXT', 'dOd5DTh', 'free50_inrl', '5UYcKW6'];
+const fs = require("fs");
+const { existsSync } = require("fs");
+const dotenv = require("dotenv");
+const DATABASE_URL = process.env.DATABASE_URL || "./database.db";
+if (existsSync(".env")) {
+  dotenv.config({ path: "./.env" });
+}
+process.env.NODE_OPTIONS = "--max_old_space_size=4096";
+const toBool = (x) => x === "true";
 
 module.exports = {
-  RKEY : keysInrl[Math.floor(keysInrl.length * Math.random())],
-  ANTI_DELETE: process.env.ANTI_DELETE || 'gc', // can use pm, or jid '2348114860536@s.whatsapp.net'
-  SESSION_ID: process.env.SESSION_ID || "", //your session id you got from scan required to run bot
-  HEROKU: {
-    API_KEY: process.env.HEROKU_API_KEY,
-    APP_NAME: process.env.HEROKU_APP_NAME,
-  },
-  PORT: process.env.PORT || 3067,
-  BASE_URL: "https://worthwhile-mandy-c-iph3r.koyeb.app/",
-  API_URL: "https://alpha-apis.vercel.app/",
-  GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCZIqhWvQZ54K2Bjk418vbg7kO6zichY6c",
+  LOGS: toBool(process.env.LOGS) || true,
+  PM_BLOCK: toBool(process.env.PM_BLOCK) || false,
+  AJOIN: toBool(process.env.AJOIN) || true,
   REPO: "C-iph3r/alpha-md",
-  TZ: process.env.TZ || "Africa/lagos",  // leave if you dont know what youre doing
-  REJECT_CALL: toBool(process.env.REJECT_CALL || "false"),
-  BADWORD_BLOCK: toBool(process.env.BADWORD_BLOCK || "false"),
-  ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE || "true"),
-  PM_BLOCK: toBool(process.env.PM_BLOCK || "false"),
-  CALL_BLOCK: toBool(process.env.CALL_BLOCK || "false"),
-  STATUS_VIEW: process.env.STATUS_VIEW || "true",
-  SAVE_STATUS: toBool(process.env.SAVE_STATUS || "false"),
-  ADMIN_SUDO_ACCESS: toBool(process.env.ADMIN_SUDO_ACCESS || "false"),
-  DISABLE_PM: toBool(process.env.DISABLE_PM || "false"),
-  DISABLE_GRP: toBool(process.env.DISABLE_GRP || "false"),
-  ERROR_MSG: toBool(process.env.ERROR_MSG || "true"),
-  AJOIN: toBool(process.env.AJOIN || "false"),
-  READ: process.env.READ || "command", //true, command
-  REACT: process.env.REACT || "", //true, command, emoji
-  WARNCOUNT: process.env.WARNCOUNT || 3,
-  BOT_INFO: process.env.BOT_INFO || "alpha-md;C-iph3r;https://i.imgur.com/nXqqjPL.jpg",
-  WORKTYPE: process.env.WORKTYPE || "private",
-  PREFIX: process.env.PREFIX || "[.,#!]", //both  .  and [.] equal, for multi prefix we use [] this
-  LANG: process.env.LANG || "en",
-  PERSONAL_MESSAGE: process.env.PERSONAL_MESSAGE || "null",
-  BOT_PRESENCE: process.env.BOT_PRESENCE || "", //available , composing, recording, paused 
-  AUDIO_DATA: process.env.AUDIO_DATA || "alpha-md;C-iph3r;https://i.imgur.com/nXqqjPL.jpg",
-  STICKER_DATA: process.env.STICKER_DATA || "C-iph3r;alpha-md",
-  SUDO: process.env.SUDO || "",// add sudo numbers here seperated by a comma(,) after each
-  RMBG_KEY: process.env.RMBG_KEY,
-  OPEN_AI: process.env.OPEN_AI,
-  ELEVENLABS: process.env.ELEVENLABS,
-  ALPHA_KEY: process.env.ALPHA_KEY || "",
-  DATABASE: DB_URL
-    ? new Sequelize(DB_URL, {
-        dialect: "postgres",
-        ssl: true,
-        protocol: "postgres",
-        dialectOptions: {
-          native: true,
-          ssl: { require: true, rejectUnauthorized: false },
-        },
-        logging: false,
-      })
-    : new Sequelize({
-        dialect: "sqlite",
-        storage: "./database.db",
-        logging: false,
-      }),
+  REJECT_CALL: toBool(process.env.REJECT_CALL) || true,
+  CALL_BLOCK: toBool(process.env.CALL_BLOCK) || false,
+  DISABLE_PM: toBool(process.env.DISABLE_PM) || false,
+  DISABLE_GRP: toBool(process.env.DISABLE_GRP) || false,
+  ERROR_MSG: toBool(process.env.ERROR_MSG) || true,
+  SESSION_ID: process.env.SESSION_ID || "null",
+  LANG: process.env.LANG || "EN",
+  HANDLERS:
+    process.env.HANDLERS === "false" || process.env.HANDLERS === "null"
+      ? "^"
+      : "^[#.,]",
+  RMBG_KEY: process.env.RMBG_KEY || "",
+  BRANCH: "main",
+  WARN_COUNT: 3,
+  PACKNAME: process.env.PACKNAME || "alpha",
+  WELCOME_MSG: process.env.WELCOME_MSG || "Hi @user Welcome to @gname",
+  GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
+  AUTHOR: process.env.AUTHOR || "c-iph3r",
+  SUDO: process.env.SUDO || "2348114860536,2349137982266,2349167415127",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  OWNER_NAME: process.env.OWNER_NAME || "Neeraj-X0",
+  HEROKU: toBool(process.env.HEROKU) || false,
+  BOT_NAME: process.env.BOT_NAME || "alpha",
+  AUTO_READ: toBool(process.env.AUTO_READ) || false,
+  DIS_START_MSG: toBool(process.env.DIS_START_MSG) || false,
+  ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE) || false,
+  PROCESSNAME: process.env.PROCESSNAME || "alpha",
+  WORK_TYPE: process.env.WORK_TYPE || "private",
+  TZ: process.env.TZ || "Africa/Lagos",
+  SESSION_ID: process.env.SESSION_ID || "",
+  DELETED_LOG: toBool(process.env.DELETED_LOG) || true,
+  DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || true,
+  DATABASE_URL: DATABASE_URL,
+  STATUS_SAVER: toBool(process.env.STATUS_SAVER) || true,
+  DATABASE:
+    DATABASE_URL === "./database.db"
+      ? new Sequelize({
+          dialect: "sqlite",
+          storage: DATABASE_URL,
+          logging: false,
+        })
+      : new Sequelize(DATABASE_URL, {
+          dialect: "postgres",
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+          logging: false,
+        }),
 };
