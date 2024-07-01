be42b0db9fc74fb105500063f5b701db4e30e665
Obasuyi Promise <x741822@gmail.com>
2024-07-01 15:41:39 +0000

    FEAT: updating..

--- plugins/anti_sticker.js
+++ plugins/anti_sticker.js
@@ -81,65 +81,63 @@ alpha({
 },
 async (message, match) => {
     try {
+        if (!message.isBaileys || !message.isGroup) return;
         const chatId = message.jid;
         const sudoList = config.SUDO.split(',').map(Number);
-        const senderId = message.key.participant.split("@")[0];
         const userId = message.key.participant;
-
+        const senderId = message.key.participant.split("@")[0];
         if (sudoList.includes(Number(senderId))) return;
         const isYouAdmin = await isAdmin(chatId, userId, message.client);
         const isMeAdmin = await isAdmin(chatId, message.user, message.client);
-        if (message.isBaileys && message.isGroup) {
-            const { antibot } = await groupDB(["antibot"], { jid: message.jid, content: {} }, "get");
-            if (antibot && antibot.status && antibot.action) {
-                let { key } = await message.reply("*_Bot Detected!_*");
-                await sleep(1000);
-
-                if (isYouAdmin) {
+        
+        const { antibot } = await groupDB(["antibot"], { jid: message.jid, content: {} }, "get");
+        if (!antibot || !antibot.status || !antibot.action) return;
+        let { key } = await message.reply("*_Bot Detected!_*");
+        await sleep(1000);
+        if (isYouAdmin) {
+            await message.client.sendMessage(chatId, {
+                text: "_Verified Admin!_",
+                edit: key
+            });
+        } else if (!isMeAdmin) {
+            return;
+        } else {
+            if (antibot.action === "kick") {
+                await sleep(500);
+                await message.client.groupParticipantsUpdate(chatId, userId, "remove");
+                await sleep(500);
+                await message.client.sendMessage(chatId, { delete: message.key });
+            } else if (antibot.action === "warn") {
+                const reason = "anti bot";
+                const warnInfo = await getWarns(userId);
+                let userWarnCount = warnInfo ? warnInfo.warnCount : 0;
+                userWarnCount += 1;
+                if (userWarnCount >= WARN_COUNT) {
                     await message.client.sendMessage(chatId, {
-                        text: "_Verified Admin!_",
-                        edit: key
+                        text: `_Warn limit exceeded. Kicking user @${userId.split("@")[0]}_`,
+                        mentions: [userId]
                     });
-                } else if (!isMeAdmin) {
-                    return;
+                    await sleep(500);
+                    await resetWarn(userId);
+                    await message.client.groupParticipantsUpdate(chatId, userId, "remove");
                 } else {
-                    if (antibot.action === "kick") {
-                        await sleep(500);
-                        await message.client.groupParticipantsUpdate(chatId, userId, "remove");
-                        await sleep(500);
-                        await message.client.sendMessage(chatId, { delete: message.key });
-                    } else if (antibot.action === "warn") {
-                        const reason = "anti bot";
-                        const warnInfo = await getWarns(userId);
-                        let userWarnCount = warnInfo ? warnInfo.warnCount : 0;
-                        userWarnCount += 1;
-                        if (userWarnCount >= WARN_COUNT) {
-                            await message.client.sendMessage(chatId, {
-                                text: `_Warn limit exceeded. Kicking user @${userId.split("@")[0]}_`,
-                                mentions: [userId]
-                            });
-                            await sleep(500);
-                            await resetWarn(userId);
-                            await message.client.groupParticipantsUpdate(chatId, userId, "remove");
-                        } else {
-                            await saveWarn(userId, reason);
-                            await message.client.sendMessage(chatId, {
-                                text: `_Warning_\nUser @${userId.split("@")[0]} warned.\nWarn Count: ${userWarnCount}.\nReason: ${reason}`,
-                                mentions: [userId]
-                            });
-                            await sleep(500);
-                            await message.client.sendMessage(chatId, { delete: message.key });
-                        }
-                    } else if (antibot.action === "null") {
-                        await sleep(500);
-                        await message.client.sendMessage(chatId, { delete: message.key });
-                    }
+                    await saveWarn(userId, reason);
+                    await message.client.sendMessage(chatId, {
+                        text: `_Warning_\nUser @${userId.split("@")[0]} warned.\nWarn Count: ${userWarnCount}.\nReason: ${reason}`,
+                        mentions: [userId]
+                    });
+                    await sleep(500);
+                    await message.client.sendMessage(chatId, { delete: message.key });
                 }
+            } else if (antibot.action === "null") {
+                await sleep(500);
+                await message.client.sendMessage(chatId, { delete: message.key });
             }
         }
     } catch (error) {
         errorHandler(message, error);
     }
 });
+
 
 
